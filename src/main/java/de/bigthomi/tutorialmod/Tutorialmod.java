75aa31a9d931b03c707eb673fd50c3850de31682
Thomas Reimpell <thomas.reimpell@gmail.com>
2021-12-26 15:26:14 +0100

    s

--- src/main/java/de/bigthomi/tutorialmod/Tutorialmod.java
+++ src/main/java/de/bigthomi/tutorialmod/Tutorialmod.java
@@ -1,13 +1,18 @@
 package de.bigthomi.tutorialmod;
 
+import de.bigthomi.tutorialmod.core.init.BlockInit;
 import de.bigthomi.tutorialmod.core.init.ItemInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -22,6 +27,7 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("tutorialmod")
+@Mod.EventBusSubscriber(modid = Tutorialmod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Tutorialmod {
 
     // Directly reference a log4j logger.
@@ -30,10 +36,19 @@ public class Tutorialmod {
     public Tutorialmod() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        BlockInit.BLOCKS.register(bus);
         ItemInit.ITEMS.register(bus);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    @SubscribeEvent
+    public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
+        BlockInit.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block ->
+        {
+            event.getRegistry().register(new BlockItem(block, new Item.Properties().group(ItemGroup.REDSTONE))
+                    .setRegistryName(block.getRegistryName()));
+        });
+    }
 
 }
