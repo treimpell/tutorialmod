df3eccf5215395c233fca631641c93d22eaef3cc
Thomas Reimpell <thomas.reimpell@gmail.com>
2021-12-26 14:46:35 +0100

    s

--- src/main/java/de/bigthomi/tutorialmod/core/init/ItemInit.java
+++ src/main/java/de/bigthomi/tutorialmod/core/init/ItemInit.java
@@ -3,6 +3,7 @@ package de.bigthomi.tutorialmod.core.init;
 import de.bigthomi.tutorialmod.Tutorialmod;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Rarity;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,5 +12,7 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tutorialmod.MOD_ID);
 
     public static final RegistryObject<Item> TEST_INGOT = ITEMS.register("test_ingot",
-            ()->new Item(new Item.Properties().group(ItemGroup.REDSTONE)));
+            ()->new Item(new Item.Properties().group(ItemGroup.REDSTONE).rarity(Rarity.EPIC)));
+
+
 }
