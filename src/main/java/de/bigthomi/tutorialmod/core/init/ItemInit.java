1325dc7f24fbea0c981778d59b094772f4401217
Thomas Reimpell <thomas.reimpell@gmail.com>
2021-12-26 13:11:29 +0100

    Initial commit

---
+++ src/main/java/de/bigthomi/tutorialmod/core/init/ItemInit.java
@@ -0,0 +1,15 @@
+package de.bigthomi.tutorialmod.core.init;
+
+import de.bigthomi.tutorialmod.Tutorialmod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tutorialmod.MOD_ID);
+
+    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item",
+            ()->new Item(new Item.Properties().group(ItemGroup.REDSTONE)));
+}
