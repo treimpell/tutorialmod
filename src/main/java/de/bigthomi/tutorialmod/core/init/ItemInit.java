073413cf8b05ab79704e4bf6088ebcc310344e0c
Thomas Reimpell <thomas.reimpell@gmail.com>
2021-12-26 14:32:07 +0100

    s

--- src/main/java/de/bigthomi/tutorialmod/core/init/ItemInit.java
+++ src/main/java/de/bigthomi/tutorialmod/core/init/ItemInit.java
@@ -10,6 +10,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tutorialmod.MOD_ID);
 
-    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item",
+    public static final RegistryObject<Item> TEST_INGOT = ITEMS.register("test_ingot",
             ()->new Item(new Item.Properties().group(ItemGroup.REDSTONE)));
 }
